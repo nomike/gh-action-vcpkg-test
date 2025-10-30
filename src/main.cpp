fd27638a0723ed85d88bed4e6274e5de45c0e4dc
nomike <nomike@nomike.com>
2025-10-30 17:37:50 +0100

    Refactor version output to use GetLibraryVersion for detailed lib3mf version information

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,11 @@ int main() {
         
         std::cout << "Hello world!" << std::endl;
         std::cout << "lib3mf model created successfully!" << std::endl;
-        std::cout << "Version: " << pWrapper->GetVersion() << std::endl;
+        
+        // Get version information from the wrapper
+        Lib3MF_uint32 nMajor, nMinor, nMicro;
+        pWrapper->GetLibraryVersion(nMajor, nMinor, nMicro);
+        std::cout << "lib3mf Version: " << nMajor << "." << nMinor << "." << nMicro << std::endl;
         
         return 0;
     }
